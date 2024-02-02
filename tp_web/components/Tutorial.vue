ede7ac894cfae2fdbd0aa66e2a5c837f1acc34da
tyeon99 <xodus170@naver.com>
2024-02-02 14:56:04 +0900

    기업씽크풀 웹 테스트

---
+++ tp_web/components/Tutorial.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="mt-4">
+    안녕하세요
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+}
+</script>
